f98151a644ff29917b1980519044e3c57d13772d
thevpc <taha.bensalah@gmail.com>
2021-06-23 23:56:35 +0100

    fixed install regression

--- shef/src/main/java/net/atlanticbb/tantlinger/ui/text/actions/HTMLFontAction.java
+++ shef/src/main/java/net/atlanticbb/tantlinger/ui/text/actions/HTMLFontAction.java
@@ -4,32 +4,19 @@
  */
 package net.atlanticbb.tantlinger.ui.text.actions;
 
-import java.awt.Dialog;
-import java.awt.Frame;
-import java.awt.Window;
-import java.awt.event.ActionEvent;
-
-import javax.swing.JEditorPane;
-import javax.swing.SwingUtilities;
-import javax.swing.text.AttributeSet;
-import javax.swing.text.Element;
-import javax.swing.text.JTextComponent;
-import javax.swing.text.MutableAttributeSet;
-import javax.swing.text.SimpleAttributeSet;
-import javax.swing.text.StyleConstants;
-import javax.swing.text.html.HTMLDocument;
-
-import net.atlanticbb.tantlinger.ui.text.CompoundUndoManager;
-import net.thevpc.more.shef.HTMLUtils;
 import net.atlanticbb.tantlinger.ui.text.dialogs.HTMLFontDialog;
 import net.thevpc.more.shef.FontDesc;
 import net.thevpc.more.shef.ShefHelper;
+
+import javax.swing.*;
+import javax.swing.text.JTextComponent;
+import java.awt.*;
+import java.awt.event.ActionEvent;
 
 /**
  * Action which edits an HTML font
  *
  * @author Bob Tantlinger
- *
  */
 public class HTMLFontAction extends HTMLTextEditAction {
 
@@ -42,23 +29,13 @@ public class HTMLFontAction extends HTMLTextEditAction {
         super(i18n.str("font_"));         //$NON-NLS-1$
     }
 
-    protected void sourceEditPerformed(ActionEvent e, JEditorPane editor) {
-        HTMLFontDialog d = createDialog(editor);
-        d.setLocationRelativeTo(d.getParent());
-        d.setVisible(true);
-        if (!d.hasUserCancelled()) {
-            editor.requestFocusInWindow();
-            editor.replaceSelection(d.getHTML());
-        }
-    }
-
     protected void wysiwygEditPerformed(ActionEvent e, JEditorPane editor) {
         HTMLFontDialog d = createDialog(editor);
         FontDesc font = ShefHelper.runGetFont(editor);
 
         d.setBold(font.isBold());
         d.setItalic(font.isItalic());
-        d.setUnderline(font.isUnderline());
+        d.setUnderline(font.getUnderline() != null && font.getUnderline());
         if (font.getFontName() != null) {
             d.setFontName(font.getFontName());
         }
@@ -70,6 +47,16 @@ public class HTMLFontAction extends HTMLTextEditAction {
         if (!d.hasUserCancelled()) {
             ShefHelper.runChangeFont(editor, new FontDesc(d.getFontName(), d.getFontSize(),
                     d.isBold(), d.isItalic(), d.isUnderline()));
+        }
+    }
+
+    protected void sourceEditPerformed(ActionEvent e, JEditorPane editor) {
+        HTMLFontDialog d = createDialog(editor);
+        d.setLocationRelativeTo(d.getParent());
+        d.setVisible(true);
+        if (!d.hasUserCancelled()) {
+            editor.requestFocusInWindow();
+            editor.replaceSelection(d.getHTML());
         }
     }
 
